b7375f82ad0e5b0d91e6e101ebfe17329ae2fda7
Alba Osmanaj <albaos91@gmail.com>
2020-09-25 22:08:40 -0500

    make HTML quizzes work

--- js/app.js
+++ js/app.js
@@ -1,132 +1,82 @@
+const QuizGame = () => {
+  // Set time
+  let totalTime = 5;
+  let gameTime = 0;
 
-// declare all elements //
+  // Set scores
+  let correct = 0;
+  let wrong = 0;
+  const total = questions.length;
 
-const startQ = document.querySelector("#startQ");
-const quiz = document.querySelector("#quiz");
-const question = document.querySelector("#question");
-const QImg = document.querySelector("#QImg");
-const choiceA = document.querySelector("#A");
-const choiceB = document.querySelector("#B");
-const choiceC = document.querySelector("#C");
-const counter = document.querySelector("#counter");
-const timeGauge = document.querySelector("#timeGauge");
-const progress = document.querySelector("#progress"); 
-const score = document.querySelector("#score"); 
+  const init = () => {
+    $(document).ready(function () {
+      // console.log(questions);
+      $(".html").on("click", loadHTMLCards);
+      $(".css").on("click", loadCSSCards);
+      $(".js").on("click", loadJsCards);
+      // $('.js_intro').on('click', startGame);
+      $(".scores_nav").on("click", checkRankingSubmit);
+      $(".scores").on("click", checkRankingSubmit);
+    });
+  };
 
-//  create questions  //
+  const startGame = (e) => {
+    $(".quiz_cards").hide();
+    $(".scores").show();
+    $(".time").show();
+    $(".quiz").show();
 
-let questions = [
-    {
-        question : "What is HTML?", 
-        imgSrc : "assets/html_icon.png",
-        choiceA : "What you see on the browser",
-        choiceB : "The standard markup language for documents designed to be displayed in a web browser",
-        choiceC : "The standard markup language for things designed to be displayed ont the phone browser",
-        correct : "B"
-    }, {
-        question : "What does HTML stand for?", 
-        imgSrc : "assets/html_icon.png",
-        choiceA : "Hypertext Markup Language",
-        choiceB : "Hypertext Markdown Language",
-        choiceC : "Hello There My Love",
-        correct : "A"
-    }, {
-        question : "What is CSS?", 
-        imgSrc : "assets/css_icon.jpg",
-        choiceA : "A style sheet language used for writing the presentation of an object written in a markup language like HTML",
-        choiceB : "A style sheet language used for describing the presentation of a document written in a markup language like HTML",
-        choiceC : "It is something about styling",
-        correct : "B"
-    }, {
-        question : "What does CSS stand for?", 
-        imgSrc : "assets/css_icon.jpg",
-        choiceA : "Cursor Style Sheets",
-        choiceB : "Creative Style Sheets",
-        choiceC : "Cascading Style Sheets",
-        correct : "C"
-    }, {
-        question : "What is Javascript used for?", 
-        imgSrc : "assets/js_icon.png",
-        choiceA : "JavaScript is used to create elements for web pages",
-        choiceB : "JavaScript is used to create responsive, interactive elements for web pages, enhancing the user experience",
-        choiceC : "JavaScript is used to create responsive, interactive elements for web pages, and to make your life harder",
-        correct : "B"
-    }, {
-        question : "What is the real name of Javascript?", 
-        imgSrc : "assets/js_icon.png",
-        choiceA : "ECMAScript",
-        choiceB : "EICHScript",
-        choiceC : "EMCAScript",
-        correct : "A"
-    }
-];
+    // Change bg color
+    document.getElementsByTagName("nav")[0].style.borderBottom =
+      "1px solid #7DE38D";
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#00242B";
+    displayQuestion();
 
-const lastQuestion = questions. length -1;
-let runningQ = 0;
-let count = 0;
-const questionTime = 10;
-const gaugeWidth = 150; 
-const gaugeUnit = gaugeWidth / questionTime;
-let timer;
+    $(".time").text(totalTime);
+    stopWatch();
+  };
 
-//   start the quiz   //
+  const loadHTMLCards = () => {
+    // HTML Quizzes
+    const htmlQuizzes = [
+      {
+        title: "HTML Tags",
+        intro: "HTML elements and structure, the building blocks of websites.",
+        id: "html_tags",
+      },
+      {
+        title: "HTML Tables",
+        intro: "the syntax you need to create tables in your HTML documents.",
+        id: "html_tables",
+      },
+      {
+        title: "HTML Forms",
+        intro: "the inner workings of an HTML form!",
+        id: "html_forms",
+      },
+    ];
 
-startQ.addEventListener("click", startQuiz);
+    $(".catalog").hide();
+    $(".languages").hide();
+    $(".outro").hide();
+    $(".quiz_cards").show();
 
-function startQuiz(){
-    startQ.style.display = "none";
-    runQuestion();
-    quiz.style.display = "block";
-    Qprogress();
-    Qcounter();
-    timer = setInterval(Qcounter,1000);
-    let score = 0;
-}
+    htmlQuizzes.forEach((quiz) => {
+      $(".quiz_cards .container").append(`<div class="card intro mt-3">
+        <div class= "card-header">
+          <h6 class="mt-1 mb-0 ml-1">Getting Started with JavaScript</h6>
+          <img src="./src/public/assets/images/drawkit-content-man-colour.svg" alt="">
+            </div>
+          <div class="card-body">
+            <h4 class="card-title">${quiz.title}</h4>
+            <p class="card-text">Practice ${quiz.intro}</p>
+          </div>
+          <div class="card-footer">
+            <img src="./src/public/assets/images/isolated-layout.svg" alt="">
+              <p id="${quiz.id}" class="start">Practice</p>
+            </div>`);
+      $("#" + quiz.id).on("click", startGame);
+    });
+  };
 
-//   function to run the questions   //
-
-function runQuestion(){
-    let q = question[runningQ];
-    question.innerHTML = "<p>" + q.question + "</p>";
-    QImg.innerHTML = "<img src="+ q.imgSrc + ">";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-}
-
-//   list the question progress   //
-
-function Qprogress(){
-    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-        Qprogress.innerHTML += "<div class='prog' id="+qIndex+"></div>";
-    }
-}
-
-//   list correct answers   //
-
-function checkAnswer(answer){
-    if(answer == questions[runningQ].correct) {
-        score ++;
-    } else {timer('decrement()', 2000);}
-    count = 0;
-    if(runningQ < lastQuestion){
-        runningQ++;
-        runQuestion();
-    } else{
-        clearInterval(timer);
-        score();
-    }
-}
-
-
-//   create the time counter   //
-
-function Qcounter () {
-    if(count <= questionTime) {
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    } else {
-        count = 0;
-    }
-}
+  
